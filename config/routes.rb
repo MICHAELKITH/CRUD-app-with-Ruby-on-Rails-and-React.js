974da48f60fef0cc823c3ee7089162c9d8238b71
MICHAELKITH <michaelkithinji27@gmail.com>
2023-12-09 21:49:45 +0300

    Add airlines controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'pages#index'
+
+  
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+  get '*path', to: 'pages#index', via: :all
 end
