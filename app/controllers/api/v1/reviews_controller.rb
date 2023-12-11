cab5335a9e47f56d372320dc6e92a7e54b6320c7
MICHAELKITH <michaelkithinji27@gmail.com>
2023-12-11 11:40:41 +0300

    Add reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,35 @@
 module Api
     module V1
-        class ReviewsController< ApplicationController
-
+      class ReviewsController < ApplicationController
+        protect_from_forgery with: :null_session
+  
+        def create
+          review = Review.new(review_params)
+  
+          if review.save
+            render json: ReviewSerializer.new(review).serialized_json
+          else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+          end
         end
+  
+        def destroy
+          review = current_user.reviews.find(params[:id])
+  
+          if review.destroy
+            head :no_content
+          else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+  
+        private
+  
+        # Strong params
+        def review_params
+          params.require(:review).permit(:title, :description, :score, :airline_id)
+        end
+      end
     end
-end
+  end
+  
